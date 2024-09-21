4373b688fc3cc81354b3d7c2abad6aa81421dcff
Filip Mårtensson <fenwikk@protonmail.com>
2024-09-21 17:05:26 +0200

    dashboard layout

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,116 @@
+<script lang="ts">
+	import CircleUser from 'lucide-svelte/icons/circle-user';
+	import Menu from 'lucide-svelte/icons/menu';
+	import Package2 from 'lucide-svelte/icons/package-2';
+	import Search from 'lucide-svelte/icons/search';
+
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { Button } from '$lib/components/ui/button';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import { Input } from '$lib/components/ui/input';
+	import * as Sheet from '$lib/components/ui/sheet';
+
+	const navItems: { name: string; href: string }[] = [
+		{ name: 'Dashboard', href: '/dashboard' },
+		{ name: 'Analytics', href: '/dashboard/analytics' },
+		{ name: 'Settings', href: '/dashboard/settings' }
+	];
+</script>
+
+<div class="flex min-h-screen w-full flex-col">
+	<header class="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+		<nav
+			class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6"
+		>
+			<a href="/" class="flex items-center gap-2 text-lg font-semibold md:text-base">
+				<Package2 class="h-6 w-6" />
+				<span class="sr-only">Acme Inc</span>
+			</a>
+
+			{#each navItems as { name, href }}
+				<a
+					{href}
+					class="transition-colors hover:text-foreground {(
+						href === '/dashboard'
+							? $page.url.pathname === '/dashboard'
+							: $page.url.pathname.startsWith(href)
+					)
+						? 'text-foreground'
+						: 'text-muted-foreground'}"
+				>
+					{name}
+				</a>
+			{/each}
+		</nav>
+		<Sheet.Root>
+			<Sheet.Trigger asChild let:builder>
+				<Button variant="outline" size="icon" class="shrink-0 md:hidden" builders={[builder]}>
+					<Menu class="h-5 w-5" />
+					<span class="sr-only">Toggle navigation menu</span>
+				</Button>
+			</Sheet.Trigger>
+			<Sheet.Content side="left">
+				<nav class="grid gap-6 text-lg font-medium">
+					<a href="/" class="flex items-center gap-2 text-lg font-semibold">
+						<Package2 class="h-6 w-6" />
+						<span class="sr-only">Acme Inc</span>
+					</a>
+
+					{#each navItems as { name, href }}
+						<a
+							{href}
+							class="transition-colors hover:text-foreground {(
+								href === '/dashboard'
+									? $page.url.pathname === '/dashboard'
+									: $page.url.pathname.startsWith(href)
+							)
+								? 'text-foreground'
+								: 'text-muted-foreground'}"
+						>
+							{name}
+						</a>
+					{/each}
+				</nav>
+			</Sheet.Content>
+		</Sheet.Root>
+		<div class="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+			<form class="ml-auto flex-1 sm:flex-initial">
+				<div class="relative">
+					<Search class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+					<Input
+						type="search"
+						placeholder="Search products..."
+						class="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+					/>
+				</div>
+			</form>
+			<DropdownMenu.Root>
+				<DropdownMenu.Trigger asChild let:builder>
+					<Button builders={[builder]} variant="secondary" size="icon" class="rounded-full">
+						<CircleUser class="h-5 w-5" />
+						<span class="sr-only">Toggle user menu</span>
+					</Button>
+				</DropdownMenu.Trigger>
+				<DropdownMenu.Content align="end">
+					<DropdownMenu.Label>My Account</DropdownMenu.Label>
+					<DropdownMenu.Separator />
+					<a href="/dashboard/me">
+						<DropdownMenu.Item>Profile</DropdownMenu.Item>
+					</a>
+					<a href="/dashboard/me/settings">
+						<DropdownMenu.Item>Settings</DropdownMenu.Item>
+					</a>
+
+					<DropdownMenu.Separator />
+					<DropdownMenu.Item>Logout</DropdownMenu.Item>
+				</DropdownMenu.Content>
+			</DropdownMenu.Root>
+		</div>
+	</header>
+	<main
+		class="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10"
+	>
+		<slot />
+	</main>
+</div>
