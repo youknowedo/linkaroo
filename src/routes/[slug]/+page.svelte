7660cb0c5abee4c66f60785ae691622564e984ef
Filip Mårtensson <fenwikk@protonmail.com>
2024-09-25 20:41:22 +0200

    show other pages

---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	import { Builder } from "$lib/builder";
+	import Page from "$lib/components/Page.svelte";
+	import { builder } from "$lib/stores";
+	import { trpc } from "$lib/trpc/client";
+	import { onMount } from "svelte";
+
+	onMount(async () => {
+		const {
+			success,
+			error,
+			page: p
+		} = await trpc($page).pages.single.query({
+			slug: $page.params.slug
+		});
+
+		if (success && p) builder.set(new Builder(p.blocks));
+		else console.error(error);
+	});
+</script>
+
+<Page />
